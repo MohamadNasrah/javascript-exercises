fdf4367e9f67a9ccf2e82be302b3b5c3d1e54f50
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-07 22:40:36 +0300

    Solve JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let original = string;
+let splitted = original.split("");
+let reversed = [];
+for (let i = splitted.length ; i >= 0; i-- ) {
+ reversed.push(splitted[i]);
+}
+let fixed = reversed.join("")
+return fixed;
 
 };
 
