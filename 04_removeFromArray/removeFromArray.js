63356ff852f3a463f2eba2ebe8548d8bd9299786
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-08 17:18:09 +0300

    'solve first test in JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrs, itm) {
+  let buffer = arrs;
+  let result = [];
 
+  for (const arr of buffer) {
+    if (arr === itm) {result = buffer.splice(buffer.indexOf(arr),1)}
+  }
+  return buffer;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
