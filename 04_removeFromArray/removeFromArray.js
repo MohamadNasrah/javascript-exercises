27fa716422e7eece4bcf444ca31af924cbdfda2a
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-09 00:59:07 +0300

    Solve JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,36 @@
-const removeFromArray = function(arrs, itm) {
-  let buffer = arrs;
-  let result = [];
+let instances = 0;
 
-  for (const arr of buffer) {
-    if (arr === itm) {result = buffer.splice(buffer.indexOf(arr),1)}
+
+const removeFromArray = function(arrs, ...args) {
+  let savedArray = arrs.slice();
+  let result = savedArray;
+  let deleted = [];
+
+
+
+  for (const arg of args) {
+    getNumberOfInstances(result, arg);
+    for (let i = 0; i < instances; i++) {
+      for (const itm of result) {
+        if (arg === itm) {
+            deleted = result.splice(result.indexOf(itm) ,1);
+        }
+      }
+    }
+    
   }
-  return buffer;
+  instances = 0;
+  return result;
 };
 
+function getNumberOfInstances (array, insta) {
+  for (const arr of array) {
+    if (arr === insta) {instances++;}
+  }
+}
 
+
+
+// if (arr === itm) {result = buffer.splice(buffer.indexOf(arr),1)}
 // Do not edit below this line
 module.exports = removeFromArray;
