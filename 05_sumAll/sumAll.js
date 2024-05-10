d4e27449fbcd6de16212620700d2a55edbb5c060
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-10 19:17:42 +0300

    Solve JS exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let allNums = [];
+    let result = 0;
+    let numOne;
+    let numTwo;
+     
+    if (num1 <= 0 || num2 <= 0 
+        || isNaN(num1) || isNaN(num2) 
+        || typeof num1 === 'string' || typeof num2 === 'string'        
+    ) {
+        return "ERROR"
+    } else {
+        if(num1 < num2) {numOne = num1; numTwo = num2;}
+        else if (num1 > num2) {numOne = num2; numTwo = num1;}
 
+        for (let i = numOne; i <= numTwo; i++) {
+            let buffer = i;
+            allNums.push(buffer);
+        }
+        for (const num of allNums) {
+            result += num;
+        }
+        return result;
+    }
+    
 };
 
 // Do not edit below this line
