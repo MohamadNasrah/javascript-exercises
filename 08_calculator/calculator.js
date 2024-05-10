41b6312ada663447415a8596fa330199eca29782
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-11 02:12:46 +0300

    Solve JS exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2 ) {
+  return num1 - num2;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+  let result= 0;
+	for (const elm of arr) {
+    result += Number(elm);
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let result = 1;
+	for (const elm of arr) {
+    result *= Number(elm);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {return 1}
+  else {
+    let result = 1;
+    if (num === 1) {return result}
+    result = num * factorial(num -1)
+    return result;
+    
+  }
 };
 
 // Do not edit below this line
