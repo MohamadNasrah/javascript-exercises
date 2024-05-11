bbc74195376674c5bebbc433c56c1e755c45fd5d
MohamadNasrah <mohamad.nasrah@gmail.com>
2024-05-12 01:33:34 +0300

    Solve JS exercies 09-10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuation = /[\. ,?!]/g;
+    let cleaned = str.replace(punctuation, '').toLowerCase();
+    let arrayed = cleaned.split("").reverse();
+    let joined = arrayed.join("");
+    if (cleaned === joined) {
+        return true;
+    } else {return false;}
+    
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let loops = Number(num)
+    let first = 0;
+    let second = 1;
+
+    if (num < 0) return "OOPS";
+    if (Number(num) === 0) return 0;
+    for (let i = 1; i < loops; i++) {
+        let buffer = first + second;
+        first = second;
+        second = buffer;
+    }
+    return second;
 
 };
 
